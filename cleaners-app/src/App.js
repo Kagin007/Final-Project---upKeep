27e34f09aa226aff7e657821920b412e132d09f9
Adam Schulte <adamschulte148@hotmail.com>
2022-03-15 19:47:00 +0000

    Final Project initial commit

---
+++ cleaners-app/src/App.js
@@ -0,0 +1,36 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import "./App.css";
+
+
+function App() {
+
+  const backend_url = 'http://127.0.0.1:3000/api/v1/users' 
+
+  useEffect(() => {
+    axios.get(backend_url)
+        .then(res => setUsers(res.data))
+        .catch(error => console.log(error))
+  }, []);
+
+  const [users, setUsers] = useState([]);
+
+  return (
+    <div className="App">
+      <h1>Cleaners APP</h1>
+      <h2>User Names</h2>
+      <div>
+        {users.map((user,_) => {
+          return (
+            <div>
+              {user.id}. {user.name}
+            </div>
+          )
+        })}
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
