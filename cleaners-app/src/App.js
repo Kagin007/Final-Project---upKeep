e9ff27b7a8caae147759ed92e4e31d9c836f8306
Andrew Ho <andrew.gjh@gmail.com>
2022-03-18 17:41:54 -0400

    Create a basic navigation component

--- cleaners-app/src/App.js
+++ cleaners-app/src/App.js
@@ -1,37 +1,34 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
-
+import Navigation from "./components/Navigation";
 
 function App() {
-
-  const backend_url = 'http://127.0.0.1:3000/api/v1/users' 
+  const backend_url = "http://127.0.0.1:3000/api/v1/users";
 
   useEffect(() => {
-    axios.get(backend_url)
-        .then(res => setUsers(res.data))
-        .catch(error => console.log(error))
+    axios
+      .get(backend_url)
+      .then(res => setUsers(res.data))
+      .catch(error => console.log(error));
   }, []);
 
   const [users, setUsers] = useState([]);
 
   return (
     <div className="App">
-      <h1>Cleaners APP</h1>
-      <button>
-        Click for cleaners
-      </button>
+      <Navigation />
+      <button>Click for cleaners</button>
       <h2>User Names</h2>
       <div>
-        {users.map((user,_) => {
+        {users.map((user, _) => {
           return (
             <div>
               {user.id}. {user.last_name}
             </div>
-          )
+          );
         })}
       </div>
-
     </div>
   );
 }
--- cleaners-app/src/components/Navigation.jsx
+++ cleaners-app/src/components/Navigation.jsx
@@ -1,8 +1,18 @@
+import { Fragment } from "react";
+
 const Navigation = props => {
   return (
-    <nav>
-      <h1>upKeeper</h1>
-    </nav>
+    <Fragment>
+      <nav className="nav__bar">
+        <h1 className="nav__logo">upKeeper</h1>
+      </nav>
+      <ul className="nav__options">
+        <li className="nav__options--link">My Reservations</li>
+        <li className="nav__options--link">My Profile</li>
+        <li className="nav__options--link">Login</li>
+        <li className="nav__options--link">Sign Up</li>
+      </ul>
+    </Fragment>
   );
 };
 export default Navigation;
