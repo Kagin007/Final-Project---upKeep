a125123fb79370c304deae57b380336a08309714
Andrew Ho <andrew.gjh@gmail.com>
2022-03-21 17:53:28 -0400

    Resolve merge conflicts

--- cleaners-app/src/App.js
+++ cleaners-app/src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
+import React from "react";
 import Navigation from "./components/Navigation";
 import SearchForm from "./components/SearchForm";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Register from "./Register";
 import Login from "./Login";
-import Home from "./Home"
-
-
+import Home from "./Home";
 
 function App() {
   return (
     <Router>
       <Navigation />
       <Switch>
-         <Route exact path="/">
+        <Route exact path="/">
           <Home />
-          <SearchForm /> 
-           </Route> 
+          <SearchForm />
+        </Route>
         <Route exact path="/register">
           <Register />
         </Route>
--- cleaners-app/src/Home.js
+++ cleaners-app/src/Home.js
@@ -1,32 +1,34 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios"
-import "./Home.css";
+import axios from "axios";
+import "./App.css";
+import Navigation from "./components/Navigation";
+import SearchForm from "./components/SearchForm";
+import CleanersList from "./components/CleanersList";
+import Backdrop from "./components/Backddrop";
+import ReviewsModal from "./components/ReviewsModal";
+import useModal from "./hooks/useReviewsModal";
 
-export default function Home() {
+function App() {
   const backend_url = "http://127.0.0.1:3000/api/v1/users";
 
   useEffect(() => {
     axios
       .get(backend_url)
-      .then((res) => setUsers(res.data))
-      .catch((error) => console.log(error));
+      .then(res => setUsers(res.data))
+      .catch(error => console.log(error));
   }, []);
-
+  const { reviewModalOpen, toggleReviewModal } = useModal();
   const [users, setUsers] = useState([]);
 
   return (
     <div className="App">
-      <button>Click for cleaners</button>
-      <h2>User Names</h2>
-      <div>
-        {users.map((user, _) => {
-          return (
-            <div>
-              {user.id}. {user.last_name}
-            </div>
-          );
-        })}
-      </div>
+      <Navigation />
+      <SearchForm />
+      <CleanersList onOpen={toggleReviewModal} />
+      {reviewModalOpen && <Backdrop onClose={toggleReviewModal} />}
+      {reviewModalOpen && <ReviewsModal onClose={toggleReviewModal} />}
     </div>
   );
 }
+
+export default App;
