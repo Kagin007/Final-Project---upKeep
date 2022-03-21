547739ed18e5fb7f499152a4176a0761ac08b966
Andrew Ho <andrew.gjh@gmail.com>
2022-03-21 17:44:54 -0400

    Resolve merge conflict

---
+++ cleaners-app/src/Home.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+import Navigation from "./components/Navigation";
+import SearchForm from "./components/SearchForm";
+import CleanersList from "./components/CleanersList";
+import Backdrop from "./components/Backddrop";
+import ReviewsModal from "./components/ReviewsModal";
+import useModal from "./hooks/useReviewsModal";
+
+function App() {
+  const backend_url = "http://127.0.0.1:3000/api/v1/users";
+
+  useEffect(() => {
+    axios
+      .get(backend_url)
+      .then(res => setUsers(res.data))
+      .catch(error => console.log(error));
+  }, []);
+  const { reviewModalOpen, toggleReviewModal } = useModal();
+  const [users, setUsers] = useState([]);
+
+  return (
+    <div className="App">
+      <Navigation />
+      <SearchForm />
+      <CleanersList onOpen={toggleReviewModal} />
+      {reviewModalOpen && <Backdrop onClose={toggleReviewModal} />}
+      {reviewModalOpen && <ReviewsModal onClose={toggleReviewModal} />}
+    </div>
+  );
+}
+
+export default App;
