7219ad31de5d4a1e1f34a12c9fa59b7f40007359
Andrew Ho <andrew.gjh@gmail.com>
2022-03-21 19:34:08 -0400

    Update skeleton

--- upkeeper-backend/app.js
+++ upkeeper-backend/app.js
@@ -1,41 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const routes = require("./routes");
+const middlewares = require("./middlewares");
+const db = require("./db");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+// SERVER SETTINGS + MIDDLEWARES
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.use(helmet());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// ROUTES/ENDPOINTS
+// Homepage
+app.get("/", (req, res) => {
+  return res.json({ message: "Hola!" });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use("/api/v1", routes);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 module.exports = app;
