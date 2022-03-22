e22702f2718300d5f77e56e6d1ead3314a6008b2
Andrew Ho <andrew.gjh@gmail.com>
2022-03-22 10:11:48 -0400

    Clean up unused files

--- upkeeper-api/server/db/schema/01_create_users.sql
+++ upkeeper-api/server/db/schema/01_create_users.sql
@@ -2,7 +2,11 @@
 DROP TABLE IF EXISTS users CASCADE;
 -- CREATE USERS
 CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
+  id SERIAL NOT NULL PRIMARY KEY,
+  first_name VARCHAR(255) NOT NULL, 
+  last_name VARCHAR(255) NOT NULL, 
   email VARCHAR(255) NOT NULL,
-  password VARCHAR(255)
+  password VARCHAR(300) NOT NULL, 
+  picture_url VARCHAR(1000), 
+  role VARCHAR(255)
 );
