b4a99849f2b5cdeab87f23e9a77b485867be1f44
Andrew Ho <andrew.gjh@gmail.com>
2022-03-23 09:08:32 -0400

    Start cleaners review route

--- upkeeper-api/server/routes/userRoutes.js
+++ upkeeper-api/server/routes/userRoutes.js
@@ -1,5 +1,3 @@
-const { query } = require("express");
-
 const router = require("express").Router();
 
 module.exports = db => {
@@ -12,6 +10,8 @@ module.exports = db => {
   //     Promise.resolve(res.json(data.rows));
   //   });
   // });
+
+  // this route is to get an individual owner and all his propertis
   router.get("/:id", (req, res) => {
     const queryStatement = `
     SELECT * FROM users 
@@ -22,27 +22,33 @@ module.exports = db => {
       Promise.resolve(res.json(data.rows));
     });
   });
-  // router.get("/:id", (req, res) => {
-  //   const queryStatement = `
-  //   SELECT * FROM users
-  //   JOIN properties ON user_id = users.id
-  //   WHERE users.id = $1;
-  //   `;
-  //   const queryParams = [req.params.id];
-  //   db.query(queryStatement).then(data => {
-  //     Promise.resolve(res.json(data.rows));
-  //   });
-  // });
+
+  //this route is to grab all the reviews for cleaners
+  router.get("/:id/reviews", (req, res) => {
+    const queryStatement = `
+    SELECT * FROM users
+    RIGHT JOIN cleaners ON user_id = users.id
+    RIGHT JOIN ratings ON cleaner_id = cleaners.id
+    JOIN reservations ON reservations.cleaner_id = cleaners.id
+    WHERE users.id = $1;
+    `;
+    const queryParams = [req.params.id];
+    db.query(queryStatement, queryParams).then(data => {
+      Promise.resolve(res.json(data.rows));
+    });
+  });
+
+  //this route is to get all cleaners by location and the date where they don't have a booking
   router.get("/", (req, res) => {
     const queryStatement = `
-    SELECT first_name AS firstName, last_name AS lastName, city_province, picture_url AS imgURL, cleaners.payrate AS payRate, AVG(rating) AS avgRating ,COUNT(rating) AS numRatings FROM users
+    SELECT cleaners.id AS cleanerID, first_name AS firstName, last_name AS lastName, city_province, picture_url AS imgURL, cleaners.payrate AS payRate, AVG(rating) AS avgRating ,COUNT(rating) AS numRatings FROM users
     JOIN cleaners ON user_id = users.id
     JOIN locations ON location_id = locations.id
     JOIN reservations ON reservations.cleaner_id = cleaners.id
     LEFT JOIN ratings ON ratings.cleaner_id = cleaners.id
     WHERE booking_date != $1
-    GROUP BY users.id, payrate, city_province
-    HAVING city_province LIKE $2;;
+    GROUP BY users.id, payrate, city_province, cleaners.id
+    HAVING city_province LIKE $2;
     `;
     const queryParams = [`${req.query.date}`, `%${req.query.city}%`];
     db.query(queryStatement, queryParams).then(data => {
@@ -52,10 +58,3 @@ module.exports = db => {
 
   return router;
 };
-
-// SELECT * FROM cleaners
-//     JOIN users ON user_id = users.id
-//     JOIN locations ON location_id = locations.id
-//     JOIN reservations ON cleaner_id = cleaners.id
-//     JOIN ratings ON
-//     WHERE city_province ='Alert Bay, British Columbia';
