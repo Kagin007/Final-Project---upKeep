727c881bd548f94b2421d6509d7eda1ab38b6295
Rho <rhodaphillipsosei@gmail.com>
2022-03-29 13:10:30 -0400

    Add background image

--- cleaners-app/src/App.js
+++ cleaners-app/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState, useContext, createContext } from "react";
 import Navigation from "./components/Navigation";
 import SearchForm from "./components/SearchForm";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import background from "./img/background.jpg";
 import Login from "./Login";
 import Home from "./Home";
 import Profile from "./OwnerProfile";
@@ -10,7 +10,6 @@ import Profile from "./OwnerProfile";
 import axios from "axios";
 import AuthProvider from "./providers/AuthProvider";
 import ReservationsList from "./components/ReservationsList";
-
 
 function App() {
   const user = JSON.parse(window.localStorage.getItem("user"));
@@ -24,55 +23,71 @@ function App() {
       } else {
         axios
           .get(`/api/properties/${user.id}`)
-          .then(res => {
+          .then((res) => {
             setProperties(res.data);
           })
-          .catch(err => {
+          .catch((err) => {
             console.log(err);
           });
         axios
           .get(`/api/member/${user.id}`)
-          .then(res => {
+          .then((res) => {
             setMemberData(res.data);
           })
-          .catch(err => {
+          .catch((err) => {
             console.log(err);
           });
       }
     }, 400);
   }, []);
 
+  // const containerStyle = {
+  //   height: "100vh",
+  //   width: "100vw",
+  //   objectFit: "cover",
+  // };
+
+  const backgroundStyle = {
+    backgroundImage: `url(${background})`,
+    backgroundSize: "cover",
+    height: "100vh",
+    backgroundPosition: "right"
+  };
   return (
-    <AuthProvider>
-      <Router>
-        <Navigation />
-        <Switch>
-          <Route exact path="/">
-            <Home properties={properties} />
-          </Route>
-          <Route exact path="/reservations">
-            <ReservationsList />
-          </Route>
-          {/* <Route exact path="/properties">
+    <container className="container">
+      <div style={backgroundStyle}>
+        <AuthProvider>
+          <Router>
+            <Navigation />
+            <Switch>
+              <Route exact path="/">
+                <Home properties={properties} />
+              </Route>
+              <Route exact path="/reservations">
+                <ReservationsList />
+              </Route>
+              {/* <Route exact path="/properties">
             <Properties
               properties={properties}
               setProperties={setProperties}
               memberData={memberData}
             />
           </Route> */}
-          <Route exact path="/profile">
-            <Profile
-              properties={properties}
-              setProperties={setProperties}
-              memberData={memberData}
-            />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-        </Switch>
-      </Router>
-    </AuthProvider>
+              <Route exact path="/profile">
+                <Profile
+                  properties={properties}
+                  setProperties={setProperties}
+                  memberData={memberData}
+                />
+              </Route>
+              <Route exact path="/login">
+                <Login />
+              </Route>
+            </Switch>
+          </Router>
+        </AuthProvider>
+      </div>
+    </container>
   );
 }
 
